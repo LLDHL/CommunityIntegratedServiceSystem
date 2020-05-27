64eca7946a98d51a611afe144f8607aa80d67cdc
hikiea <5524458+hikiea@user.noreply.gitee.com>
2020-05-27 11:25:08 +0800

    增加了Wagger监控接口

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+
+}
