b6ec3b7b3d4caa1757023f51aeb826ab5a229430
hikiea <5524458+hikiea@user.noreply.gitee.com>
2020-05-27 11:41:50 +0800

    增加了Wagger监控接口(但是被拦截了)

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration  // 把配置类配置到 spring 配置中
+@EnableSwagger2               // 开启 Swagger2
+public class SwaggerConfig {
+
+
+}
